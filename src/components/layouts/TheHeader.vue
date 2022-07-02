be792f9abdb12d978ddc7a4997bf14f60e186606
Olexiy <serboleksiy@gmail.com>
2022-07-02 11:40:31 +0300

    Adaptive header to mobile

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -6,31 +6,39 @@
           <router-link class="header__title" :to="{ name: 'main' }">
             <h1><span>Dog</span>Collars</h1>
           </router-link>
-          <ul>
-            <li>
-              <router-link class="link" :to="{ name: 'main' }"
-                >Our Products</router-link
-              >
-            </li>
-            <template v-if="authStore.isLogin">
+          <div class="burger">
+            <input id="menu-toggle" ref="burgerToggle" type="checkbox" />
+            <label class="burger__btn" for="menu-toggle">
+              <span></span>
+            </label>
+            <ul class="burger__menubox">
               <li>
-                <router-link class="link" :to="{ name: 'cabinet' }"
-                  >Cabinet</router-link
+                <router-link class="link" :to="{ name: 'main' }"
+                  >Our Products</router-link
+                >
+              </li>
+              <template v-if="authStore.isLogin">
+                <li>
+                  <router-link class="link" :to="{ name: 'cabinet' }"
+                    >Cabinet</router-link
+                  >
+                </li>
+                <li>
+                  <base-button @click="signOut">Sign Out</base-button>
+                </li>
+              </template>
+              <li v-else>
+                <base-button :to="{ name: 'register' }" link
+                  >Sign Up</base-button
                 >
               </li>
               <li>
-                <base-button @click="signOut">Sign Out</base-button>
+                <base-button class="link__cart" :to="{ name: 'cart' }" link>
+                  <shoping-cart /> <span>Cart</span> ({{ countItems }})
+                </base-button>
               </li>
-            </template>
-            <li v-else>
-              <base-button :to="{ name: 'register' }" link>Sign Up</base-button>
-            </li>
-            <li>
-              <base-button class="link__cart" :to="{ name: 'cart' }" link>
-                <shoping-cart /> <span>Cart</span> ({{ countItems }})
-              </base-button>
-            </li>
-          </ul>
+            </ul>
+          </div>
         </nav>
       </div>
     </header>
@@ -41,19 +49,27 @@
 import ShopingCart from "../icons/ShopingCart.vue";
 import { useAuthStore } from "@/stores/auth";
 import { useCartStore } from "@/stores/cart";
-import { useRouter } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import { computed } from "@vue/reactivity";
+import { watch, ref, type InputHTMLAttributes } from "vue";
 
 const authStore = useAuthStore();
 const cartStore = useCartStore();
-const { replace } = useRouter();
+const router = useRouter();
+const route = useRoute();
+
+const burgerToggle = ref<InputHTMLAttributes | null>(null);
 
 const countItems = computed(() => cartStore.countItems);
 
 const signOut = () => {
   authStore.signOut();
-  replace("/");
+  router.replace("/");
 };
+
+watch(route, () => {
+  if (burgerToggle.value) burgerToggle.value.checked = false;
+});
 </script>
 
 <style scoped>
@@ -136,5 +152,95 @@ button {
 
 button > * {
   margin-left: 0.5em;
+}
+
+.burger > input,
+.burger__btn {
+  display: none;
+}
+
+@media (max-width: 768px) {
+  .burger > input {
+    display: inline-block;
+  }
+
+  .burger__btn {
+    display: flex;
+    align-items: center;
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 26px;
+    height: 26px;
+    cursor: pointer;
+    z-index: 1;
+  }
+
+  .burger__btn > span,
+  .burger__btn > span::before,
+  .burger__btn > span::after {
+    display: block;
+    position: absolute;
+    width: 100%;
+    height: 2px;
+    background-color: #616161;
+    transition-duration: 0.25s;
+  }
+  .burger__btn > span::before {
+    content: "";
+    top: -8px;
+  }
+  .burger__btn > span::after {
+    content: "";
+    top: 8px;
+  }
+
+  .burger__menubox {
+    display: block;
+    position: fixed;
+    visibility: hidden;
+    top: 0;
+    left: -100%;
+    width: 50%;
+    height: 100%;
+    margin: 0;
+    padding: 3em 0;
+    list-style: none;
+    background-color: var(--color-layout);
+    box-shadow: 1px 0px 6px rgba(0, 0, 0, 0.2);
+    transition-duration: 0.25s;
+  }
+
+  .burger__menubox li {
+    margin-top: 1.5em;
+  }
+
+  .link__cart {
+    display: inline-block;
+  }
+
+  .link__cart > * {
+    display: inherit;
+  }
+
+  #menu-toggle {
+    opacity: 0;
+  }
+
+  #menu-toggle:checked ~ .burger__butn > span {
+    transform: rotate(45deg);
+  }
+  #menu-toggle:checked ~ .burger__butn > span::before {
+    top: 0;
+    transform: rotate(0);
+  }
+  #menu-toggle:checked ~ .burger__butn > span::after {
+    top: 0;
+    transform: rotate(90deg);
+  }
+  #menu-toggle:checked ~ .burger__menubox {
+    visibility: visible;
+    left: 0;
+  }
 }
 </style>
